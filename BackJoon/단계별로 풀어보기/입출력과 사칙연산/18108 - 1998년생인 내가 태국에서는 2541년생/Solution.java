7f7bdcc17d228c16a724c2581c2be7a6a9a268b9
GreenShrew <90941665+GreenShrew@users.noreply.github.com>
2022-03-26 15:46:26 +0900

    baekjoon

---
+++ BackJoon/단계별로 풀어보기/입출력과 사칙연산/18108 - 1998년생인 내가 태국에서는 2541년생/Solution.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.next();
+        System.out.print(Integer.parseInt(sc.next()) - 543);
+	}
+}

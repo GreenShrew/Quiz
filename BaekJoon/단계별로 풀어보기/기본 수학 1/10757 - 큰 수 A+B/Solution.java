ece507126bc9da91a7b524bc85b43ac11fe64249
GreenShrew <90941665+GreenShrew@users.noreply.github.com>
2022-05-26 13:35:30 +0900

    BaekJoon

---
+++ BaekJoon/단계별로 풀어보기/기본 수학 1/10757 - 큰 수 A+B/Solution.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		BigInteger A = new BigInteger(st.nextToken());
+		BigInteger B = new BigInteger(st.nextToken());
+
+		A = A.add(B);
+		System.out.println(A.toString());
+	}
+}

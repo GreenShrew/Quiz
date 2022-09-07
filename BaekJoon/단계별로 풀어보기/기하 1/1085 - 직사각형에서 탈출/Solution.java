da7d963c4c4eea9dfe23871c01ede8063fe39517
GreenShrew <90941665+GreenShrew@users.noreply.github.com>
2022-09-07 22:16:29 +0900

    BaekJoon

--- BaekJoon/단계별로 풀어보기/기하 1/1085 - 직사각형에서 탈출/Solution.java
+++ BaekJoon/단계별로 풀어보기/기하 1/1085 - 직사각형에서 탈출/Solution.java
@@ -1,36 +1,24 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
-
+ 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
+ 
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int count = 0;
-		
-		
-		for(int i=0; i<N; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			boolean b = true;
-			
-			if(num == 1) {
-				continue;
-			}
-			
-			for(int j=2; j<(num/2)+1; j++) {
-				if(num%j == 0) {
-					b = false;
-					break;
-				}
-			}
-			
-			if(b) {
-				count++;
-			}
-		}
-		
-		System.out.println(count);
+        
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken());
+		int h = Integer.parseInt(st.nextToken());
+        
+		int x_min = Math.min(x, w-x);
+		int y_min = Math.min(y, h-y);
+        
+		System.out.println(Math.min(x_min, y_min));
 	}
+ 
 }

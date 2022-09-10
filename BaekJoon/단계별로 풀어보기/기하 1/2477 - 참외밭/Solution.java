3faf0bb76b68df121f5aec291409b63bda8ea1b6
GreenShrew <90941665+GreenShrew@users.noreply.github.com>
2022-09-10 17:03:44 +0900

    BaekJoon

--- BaekJoon/단계별로 풀어보기/기하 1/2477 - 참외밭/Solution.java
+++ BaekJoon/단계별로 풀어보기/기하 1/2477 - 참외밭/Solution.java
@@ -4,33 +4,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int count = 0;
-		
-		
-		for(int i=0; i<N; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			boolean b = true;
-			
-			if(num == 1) {
-				continue;
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int N=Integer.parseInt(br.readLine());
+		int[] input=new int[6];
+		int maxR=0,maxC=0;
+		int indexR=0,indexC=0;
+
+		for (int i = 0; i < 6; i++) {
+			st=new StringTokenizer(br.readLine());
+			int dir=Integer.parseInt(st.nextToken());
+			int distance=Integer.parseInt(st.nextToken());
+			if(dir==3 || dir==4) {
+				maxR=maxR<distance?distance:maxR;
+				if (maxR==distance) indexR=i;
+			}else {
+				maxC=maxC<distance?distance:maxC;
+				if (maxC==distance) indexC=i;
 			}
+			input[i]=distance;
 			
-			for(int j=2; j<(num/2)+1; j++) {
-				if(num%j == 0) {
-					b = false;
-					break;
-				}
-			}
-			
-			if(b) {
-				count++;
-			}
 		}
+		int nextR1=input[5],nextC1=input[5];
+		int nextR2=input[0],nextC2=input[0];
+		if (indexC-1>-1) nextR1=input[indexC-1];
+		if (indexC+1<6) nextR2=input[indexC+1];
+		if (indexR-1>-1) nextC1=input[indexR-1];
+		if (indexR+1<6) nextC2=input[indexR+1];
 		
-		System.out.println(count);
+		System.out.println(area*N);
+
 	}
 }

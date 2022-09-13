2ee157600c0a265f00a815d177036176d22d1608
GreenShrew <90941665+GreenShrew@users.noreply.github.com>
2022-09-13 13:14:25 +0900

    BaekJoon

--- BaekJoon/단계별로 풀어보기/기하 1/1004 - 어린 왕자/Solution.java
+++ BaekJoon/단계별로 풀어보기/기하 1/1004 - 어린 왕자/Solution.java
@@ -1,36 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+package No1000;
 
+import java.util.*;
+import java.io.*;
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int count = 0;
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		
+		int T=Integer.parseInt(br.readLine());
 		
-		for(int i=0; i<N; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			boolean b = true;
+		while(T-->0) {
+			StringTokenizer st=new StringTokenizer(br.readLine());
+			int x1=Integer.parseInt(st.nextToken());
+			int y1=Integer.parseInt(st.nextToken());
+			int x2=Integer.parseInt(st.nextToken());
+			int y2=Integer.parseInt(st.nextToken());
 			
-			if(num == 1) {
-				continue;
+			int n=Integer.parseInt(br.readLine());
+			
+			int answer=0;
+			
+			
+			for(int i=0;i<n;i++) {
+				st=new StringTokenizer(br.readLine());
+				int x=Integer.parseInt(st.nextToken());
+				int y=Integer.parseInt(st.nextToken());
+				int r=Integer.parseInt(st.nextToken());
+				
+				boolean flag1=false;
+				boolean flag2=false;
+				
+				if(Math.pow(x1-x, 2)+Math.pow(y1-y, 2)>Math.pow(r, 2)) flag1=true;
+				if(Math.pow(x2-x, 2)+Math.pow(y2-y, 2)>Math.pow(r, 2)) flag2=true;
+				
+				
+				if(flag1!=flag2) answer++;
 			}
 			
-			for(int j=2; j<(num/2)+1; j++) {
-				if(num%j == 0) {
-					b = false;
-					break;
-				}
-			}
-			
-			if(b) {
-				count++;
-			}
+			System.out.println(answer);
 		}
-		
-		System.out.println(count);
 	}
+
 }

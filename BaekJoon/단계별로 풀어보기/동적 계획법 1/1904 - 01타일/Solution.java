56e2f9bc18bcb6d538ccd73cd950e03a54a014a4
GreenShrew <90941665+GreenShrew@users.noreply.github.com>
2022-10-30 19:51:13 +0900

    BaekJoon

--- BaekJoon/단계별로 풀어보기/동적 계획법 1/1904 - 01타일/Solution.java
+++ BaekJoon/단계별로 풀어보기/동적 계획법 1/1904 - 01타일/Solution.java
@@ -1,32 +1,40 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
  
 public class Main {
  
-	public static int[] dp = new int[1000001];;
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-		
-		
-		dp[0] = 0;
-		dp[1] = 1;
-		dp[2] = 2;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
  
-		for(int i = 3; i < dp.length; i++) {
-			dp[i] = -1;
-		}
-		
+		int N = Integer.parseInt(br.readLine());
+ 
 		System.out.println(Tile(N));
-		
+ 
 	}
-	
-	public static int Tile(int N) {
-		
-		if(dp[N] == -1) {
-			dp[N] = (Tile(N - 1) + Tile((N - 2))) % 15746;
+ 
+	public static int Tile(int a) {
+ 
+		if (a == 1) {
+			return 1;
+		} 
+ 
+		if (a == 2) {
+			return 2;
 		}
-		return dp[N];
+ 
+		int val1 = 1;
+		int val2 = 2;
+		int sum = 0;
+ 
+		for (int i = 2; i < a; i++) {
+			sum = (val2 + val1) % 15746;
+			val1 = val2;
+			val2 = sum;
+		}
+ 
+		return sum;
+ 
 	}
  
 }
